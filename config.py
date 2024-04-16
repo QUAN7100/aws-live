4afa06476f45397e59d6ebbaef18e63048b03f97
QUAN7100 <164607093+QUAN7100@users.noreply.github.com>
2024-04-16 15:47:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "db-1.czcw26suk1nd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "onwenquan-employee"
+customregion = "us-east-1"
+
